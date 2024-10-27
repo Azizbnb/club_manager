e0e2be748094d27b81339d789445d12cfa6212c1
aziz <bentayeb78@hotmail.fr>
2024-10-27 17:54:33 +0100

    create migrations

---
+++ database/migrations/2024_10_27_164633_create_sessions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->id();
+            $table->string('token');
+            $table->string('ip_address');
+            $table->text('user_agent');
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('expires_at')->nullable();
+
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sessions');
+    }
+};
