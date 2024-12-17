45ee11a228bb86ceaf4df2a204c7b823ce5890f0
aziz <bentayeb78@hotmail.fr>
2024-12-17 17:22:03 +0100

    tabs sessions with user name

---
+++ app/Filament/Resources/SessionResource.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SessionResource\Pages;
+use App\Filament\Resources\SessionResource\RelationManagers;
+use App\Models\Session;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class SessionResource extends Resource
+{
+    protected static ?string $model = Session::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationLabel = 'Sessions';
+    protected static ?string $navigationGroup = 'Gestion des utilisateurs';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('user.first_name')
+                    ->label('Nom Utilisateur')
+                    ->sortable(),
+
+                Tables\Columns\TextColumn::make('id')
+                    ->label('Session ID')
+                    ->sortable(),
+
+                Tables\Columns\TextColumn::make('user_id')
+                    ->label('ID Utilisateur')
+                    ->sortable(),
+
+                Tables\Columns\TextColumn::make('ip_address')
+                    ->label('Adresse IP'),
+
+                Tables\Columns\TextColumn::make('user_agent')
+                    ->label('User Agent'),
+
+                Tables\Columns\TextColumn::make('last_activity')
+                    ->label('Dernière Activité')
+                    ->dateTime('d/m/Y H:i:s'),
+            ])
+            ->filters([
+                //
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSessions::route('/'),
+            'create' => Pages\CreateSession::route('/create'),
+            'edit' => Pages\EditSession::route('/{record}/edit'),
+        ];
+    }
+}
