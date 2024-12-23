f67ddf2465e86b578e3f51b0767de4b8b3ec1ac2
aziz <bentayeb78@hotmail.fr>
2024-12-23 16:22:17 +0100

    current user can see own doc

--- app/Filament/Resources/DocumentResource.php
+++ app/Filament/Resources/DocumentResource.php
@@ -13,11 +13,23 @@ use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
+
 class DocumentResource extends Resource
 {
     protected static ?string $model = Document::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    // Si l'utilisateur connecté a le rôle "Membre", on filtre ses propres documents
+    public static function getEloquentQuery(): Builder
+    {
+        $query = parent::getEloquentQuery();
+
+        if (auth()->user()->hasRole('Membre')) {
+            $query->where('user_id', auth()->id());
+        }
+        return $query;
+    }
 
     // ---- Affichage du formulaire ----
     public static function form(Form $form): Form
@@ -49,19 +61,34 @@ class DocumentResource extends Resource
     // ---- Affichage du tableau ----
     public static function table(Table $table): Table
     {
-        return $table->columns([
-            Tables\Columns\TextColumn::make('id')->sortable()->label('ID'),
-            Tables\Columns\TextColumn::make('type')->label('Type de document'),
+        return $table
+        ->columns([
+            Tables\Columns\TextColumn::make('type')->label('Type de document')
+                ->searchable(),
             Tables\Columns\TextColumn::make('user.first_name')->label('Utilisateur'),
             Tables\Columns\TextColumn::make('created_at')->dateTime()->label('Ajouté le'),
             Tables\Columns\TextColumn::make('file_path')
                 ->label('Fichier')
                 ->url(fn ($record) => asset('storage/' . $record->file_path))
                 ->openUrlInNewTab(),
+            
+            Tables\Columns\IconColumn::make('status')->label('Récéption')
+                ->boolean(),
         ])
+
+        ->filters([])
+
         ->actions([
-            Tables\Actions\EditAction::make(),
+            Tables\Actions\EditAction::make()
+                ->visible(fn ($record) => auth()->user()->can('edit documents')),
+            Tables\Actions\DeleteAction::make()
+                ->visible(fn ($record) => auth()->user()->can('delete documents')),
+        ])
+        ->bulkActions([
+            Tables\Actions\DeleteBulkAction::make()
+                ->visible(fn () => auth()->user()->can('delete documents')),
         ]);
+
     }
 
 
